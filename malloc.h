e04a21a8079f92b0bd2d0d4d5846cf8b427ef8c4
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-23 21:13:20 +0300

    malloc: import memory management interface

---
+++ malloc.h
@@ -0,0 +1,38 @@
+/*
+ * Memory management interface
+ *
+ * Copyright (c) 2015-2017 Alexei A. Smekalkine
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef SYS_MALLOC_H
+#define SYS_MALLOC_H  1
+
+#include <stddef.h>
+
+struct mem_domain *mem_domain_alloc (const char *name);
+void mem_domain_free (struct mem_domain *domain);
+
+enum mem_flags {
+	MEM_NOWAIT	= 1,	/* do not wait for resources    */
+	MEM_ZERO	= 2,	/* set allocated memory to zero */
+};
+
+/*
+ * The mem_alloc function allocates memory for an object whose size is
+ * specified by size in the specified domain and returns a pointer to
+ * the allocated memory that is suitably aligned for storage of any
+ * kind of object, or NULL if the request could not be satisfied.
+ *
+ * The how argument specifies operational flags: it may be bitwise-or
+ * of zero or more constants of enum mem_flags.
+ *
+ * The mem_free function releases memory region pointed to by p whose
+ * size is specified by size. This region must previously be allocated
+ * in the specified domain.
+ */
+void *mem_alloc (size_t size, struct mem_domain *domain, int how);
+void mem_free (void *p, size_t size, struct mem_domain *domain);
+
+#endif  /* SYS_MALLOC_H */
