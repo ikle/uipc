243260fcd1309f75bd180dc7b1d504060a5ac2f2
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-25 05:24:46 +0300

    Add generic type support module

---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef SYS_TYPES_H
+#define SYS_TYPES_H  1
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif  /* SYS_TYPES_H */
